287403dc95da48267b10e2e46e49077161c95f80
Vivek Verma <vivnps.verma@gmail.com>
2020-07-19 15:04:15 -0700

    2-3-2 Transform

--- part1/232.py
+++ part1/232.py
@@ -41,6 +41,7 @@ colors = np.clip(colors, 0, 511)
 
 colors = zieger[colors[:, 0], colors[:, 1]]
 
+
 def rgb2hex(rgb):
     return "#{:02x}{:02x}{:02x}".format(*map(lambda x: int(x * 255), rgb))
 
@@ -52,7 +53,7 @@ class TestTransform(Scene):
 
         for index, point in enumerate(H):
             d = Dot(list(2*point) + [0], color=rgb2hex(c),
-                radius=0.75*DEFAULT_DOT_RADIUS)
+                    radius=0.75*DEFAULT_DOT_RADIUS)
             points.add(d)
 
         plane = NumberPlane()
@@ -60,13 +61,12 @@ class TestTransform(Scene):
         self.play(Write(plane))
         self.play(Write(points))
         self.wait()
-        """
+
         self.play(
             frame.set_theta, 0,
             frame.set_phi, 0.35 * PI
         )
 
-        
         rotate = True
         frame.add_updater(
             lambda m, dt: m.become(m.rotate(-0.2 * dt)) if rotate else None
@@ -95,23 +95,22 @@ class TestTransform(Scene):
 
         self.play(Transform(points, points4), run_time=5)
         self.wait(2)
-        
+
         rotate = False
         self.play(
             frame.set_phi, 0,
             frame.set_theta, 0
         )
 
-        #self.begin_ambient_camera_rotation(rate=0.1)
-        #self.wait(10)
-        """
+        # self.begin_ambient_camera_rotation(rate=0.1)
+        # self.wait(10)
 
     def func(self, x, y):
         inp = torch.tensor([x, y], dtype=torch.float32)
         y, h, z = model(inp)
         x, y, z = z.detach().numpy()
         return 3 * np.array([x, y, 2*z])
-    
+
     def func2(self, x, y):
         inp = torch.tensor([x, y], dtype=torch.float32)
         y, h, z = model(inp)
@@ -123,72 +122,3 @@ class TestTransform(Scene):
         y, h, z = model(inp)
         x, y = y.detach().numpy()
         return 6 * np.array([x, y, 0])
-
-
-
-class Test(Scene):
-    def construct(self):
-        d = Dot(color="#dd01dd")
-
-        self.play(DrawBorderThenFill(d))
-        self.wait()
-
-
-class RandomTransform(LinearTransformationScene):
-    CONFIG = {
-        "show_basis_vectors": False,
-        "foreground_plane_kwargs": {
-            "x_line_frequency": 1,
-            "y_line_frequency": 1,
-        },
-        "relu": False,
-        "camera_class": ThreeDCamera
-    }
-
-    def construct(self):
-        points = VGroup(*[
-            Dot(2*np.array([point[0], point[1], 0]), color=rgb2hex(*colors[index]),
-                radius=0.75*DEFAULT_DOT_RADIUS) for index, point in enumerate(H)
-        ])
-
-        lines = VGroup()
-        colors2 = [RED, BLUE]
-
-        for i in range(2):
-            w1, w2 = model.linear1.weight.data[i]
-            b = model.linear1.bias.data[i]
-            xin = np.linspace(-1.5, 1.5)
-            lines.add(FunctionGraph(
-                lambda x: (-1/w2) * (w1 * x + b),
-                stroke_width=1.5 * DEFAULT_STROKE_WIDTH,
-                color=colors2[i]
-            ))
-
-        self.setup()
-        self.play(Write(points))
-        self.play(Write(lines))
-        self.wait()
-
-        self.moving_mobjects += [*points]
-
-        self.apply_nonlinear_transformation(self.func, run_time=6)
-        self.wait()
-
-        if self.relu:
-
-            self.apply_nonlinear_transformation(lambda point: list(
-                map(lambda x: max(x, 0), point)), run_time=6)
-            self.wait()
-
-            self.apply_nonlinear_transformation(self.func2, run_time=6)
-            self.wait()
-
-
-class FoldTransform(RandomTransform):
-    CONFIG = {"relu": False}
-
-    def func(self, point):
-        x, y, z = point
-        inp = torch.tensor(point[:2], dtype=torch.float32)
-        y, h, z = model(inp)
-        x, y = z.detach().numpy()
