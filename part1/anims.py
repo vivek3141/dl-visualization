0504d0746af2a670823081d4faf8b67ad55c72b4
Vivek Verma <vivnps.verma@gmail.com>
2020-08-02 01:10:21 -0700

    Part 1 Anims

--- part1/anims.py
+++ part1/anims.py
@@ -171,40 +171,61 @@ class PerceptronOne(Scene):
     }
 
     def construct(self):
+        q_width = FRAME_WIDTH/4
+
         x = ValueTracker(0)
 
         perc = PerceptronMobject(
             [1, 1, 1], arrow=True, arrow_tip_size=0.1, size=0.25, neuron_stroke_color=self.n_color)
         perc.scale(1.5)
-        perc.shift(1.5 * UP + 3.5 * LEFT)
+        perc.shift(q_width * LEFT)
 
         circ = Circle(fill_opacity=0.5, color=self.n_color,
                       radius=0.25, stroke_opacity=0)
+        circ_on = False
+
+        to_move = False
 
         def circ_updater(circle):
+            if not to_move: return
             new_circle = Circle(
-                fill_opacity=0.5 * heaviside(x.get_value() - 20),
+                fill_opacity=0.5 *
+                heaviside(x.get_value() - 20) if not circ_on else 0.5,
                 color=self.n_color,
                 radius=0.25,
                 stroke_opacity=0
             )
             new_circle.scale(1.5)
-            new_circle.shift(3.5 * LEFT + 1.5 * UP)
+            new_circle.shift(q_width * LEFT)
             circle.become(new_circle)
 
         circ.add_updater(circ_updater)
 
-        l = NumberLine(x_min=0, x_max=30, numbers_with_elongated_ticks=[], unit_size=0.15, tick_frequency=5,
+        l = NumberLine(x_min=0, x_max=30, numbers_with_elongated_ticks=[], unit_size=0.2, tick_frequency=5,
                        include_numbers=True, numbers_to_show=list(range(0, 31, 5)))
-        l.shift(5.75 * LEFT + 1.5 * DOWN)
+        # l.center()
+        l.shift((0.2 * -15 + q_width) * RIGHT)
+
+        y_disp = TexMobject("0")
+
+        def y_disp_updater(y_disp):
+            if not to_move: return
+            new_disp = TexMobject(
+                str(heaviside(x.get_value() - 20)) if not circ_on else "1"
+            )
+            new_disp.shift(1 * LEFT)
+            y_disp.become(new_disp)
+
+        y_disp.add_updater(y_disp_updater)
 
         x_disp = TexMobject("0")
 
         def x_disp_updater(x_disp):
+            if not to_move: return
             new_disp = TexMobject(
-                str(heaviside(x.get_value() - 20))
+                str(int(x.get_value())) + r"^{\circ} \text{C}"
             )
-            new_disp.shift(1 * LEFT + 1.5 * UP)
+            new_disp.shift(6.25 * LEFT)
             x_disp.become(new_disp)
 
         x_disp.add_updater(x_disp_updater)
@@ -212,16 +233,142 @@ class PerceptronOne(Scene):
         ptr = Triangle(fill_opacity=1)
 
         def ptr_updater(ptr):
+            if not to_move: return
             new_ptr = Triangle(fill_opacity=1)
             new_ptr.rotate(180 * DEGREES)
             new_ptr.scale(0.15)
-            new_ptr.shift([(x.get_value()-15) * 0.15 - 3.5, -1.6, 0])
+            new_ptr.shift(
+                [(x.get_value()) * 0.2 + (0.2 * -15 + q_width), -0.1, 0])
             ptr.become(new_ptr)
 
         ptr.add_updater(ptr_updater)
 
-        self.add(circ, perc, l, x_disp, Line(10*UP, 10*DOWN), ptr)
+        inp_title = TextMobject(r"Input Space")
+        inp_title.scale(1.5)
+        inp_title.shift(q_width * RIGHT + 3 * UP)
+
+        self.add(circ, perc, l, x_disp, ptr, y_disp, inp_title)
         self.wait()
 
-        self.play(x.increment_value, 30, rate_func=linear, run_time=6)
+        self.play(x.increment_value, 30, rate_func=linear, run_time=4)
+        self.play(x.increment_value, -30, rate_func=linear, run_time=4)
+        self.wait()
+
+        line = DashedLine(2 * UP, 2 * DOWN, stroke_width=4)
+        line.shift((20 * 0.2 + (0.2 * -15 + q_width)) * RIGHT)
+
+        active_lbl = TextMobject("Active", color=RED)
+        active_lbl.add_background_rectangle()
+        active_lbl.shift((25 * 0.2 + (0.2 * -15 + q_width)) * RIGHT)
+
+        inactive_lbl = TextMobject("Inactive")
+        inactive_lbl.add_background_rectangle()
+        inactive_lbl.shift((10 * 0.2 + (0.2 * -15 + q_width)) * RIGHT)
+
+        rect = Rectangle(height=4, width=2, color=RED, fill_opacity=0.3)
+        rect.shift((25 * 0.2 + (0.2 * -15 + q_width)) * RIGHT)
+
+        self.play(Write(rect), Write(active_lbl), Write(inactive_lbl))
+        self.play(x.increment_value, 30, rate_func=linear, run_time=4)
+        self.play(x.increment_value, -30, rate_func=linear, run_time=4)
+        self.wait()
+
+        eq = TexMobject(r"\hat{y} = H( x - 20 )",
+                        tex_to_color_map={r"H": YELLOW})
+        eq.scale(1.75)
+        eq.shift(2.5 * DOWN)
+
+        self.play(Write(eq))
+        self.wait()
+
+        rect2 = Rectangle(height=4, width=4, color=RED, fill_opacity=0.3)
+        rect2.shift((10 * 0.2 + (0.2 * -15 + q_width)) * RIGHT)
+
+        eq2 = TexMobject(r"\hat{y} = H( -x + 20 )",
+                         tex_to_color_map={r"H": YELLOW, r"-": RED})
+        eq2.scale(1.75)
+        eq2.shift(2.5 * DOWN)
+
+        circ_on = True
+
+        self.play(Transform(rect, rect2), ApplyMethod(active_lbl.shift, 0.2 * 15 * LEFT), ApplyMethod(inactive_lbl.shift, 0.2 * 15 * RIGHT),
+                  ApplyMethod(circ.set_opacity, 1))
+        self.play(Transform(eq, eq2))
+        self.wait()
+
+        list_of_ok = [perc, eq, eq2]
+
+        grp = Group(*[i for i in self.mobjects if i not in list_of_ok])
+        
+        self.play(Uncreate(grp))
+        
+        to_move = False
+        grp2 = Group(circ, perc, x_disp, y_disp)
+
+        eq3 = TexMobject(r"\hat{y} = H( mx + b )",
+                        tex_to_color_map={r"H": YELLOW, "m": RED, "b": TEAL})
+        eq3.scale(1.75)
+        eq3.shift(2.5 * DOWN)
+        
+        self.play(grp2.shift, q_width * RIGHT)
+        self.play(Transform(eq, eq3))
+        self.wait()
+
+
+class PieceWiseTwo(VGroup):
+    def __init__(self, cond1, cond2, cond3, *args, **kwargs):
+        VGroup.__init__(self, *args, **kwargs)
+        eq1 = TexMobject(r"1").shift(1 * UP)
+        eq2 = TexMobject(r"0").shift(0 * UP)
+
+        eq1.align_to(eq2, LEFT)
+
+        t1 = TexMobject(r"\text{if }" + cond1).shift(1 * UP + 2 * RIGHT)
+        t2 = TexMobject(r"\text{if }" + cond2).shift(0 * UP + 2 * RIGHT)
+
+        t1.align_to(t2, LEFT)
+
+        e = VGroup(eq1, eq2)
+        t = VGroup(t1, t2)
+        et = VGroup(e, t)
+
+        b = Brace(et, LEFT)
+        bt = b.get_tex(r"H("+cond3+") = ", tex_to_color_map={"H": YELLOW})
+        eq = VGroup(et, b, bt)
+
+        eq.center()
+        self.add(eq)
+
+
+class Heaviside(Scene):
+    def construct(self):
+        axes = Axes(
+            x_min=-3,
+            x_max=3,
+            y_min=0,
+            y_max=2,
+            axis_config={
+                "include_tip": False
+            }
+        )
+        f = VGroup(FunctionGraph(lambda x: 0, x_min=-3, x_max=0),
+                   FunctionGraph(lambda x: 1, x_min=0, x_max=3))
+
+        func = VGroup(axes, f)
+        func.center()
+        func.scale(1.5)
+        func.shift(DOWN)
+
+        eq = PieceWiseTwo(r"x \geq 0", r"x < 0", "x")
+        eq.shift(2.5 * UP)
+
+        self.play(Write(func), Write(eq))
+        self.wait()
+
+        self.play(Uncreate(func), ApplyMethod(eq.shift, 2.5 * DOWN))
+
+        eq2 = PieceWiseTwo(r"x-20 \geq 0", "x-20 < 0", "x-20")
+        eq2.scale(1.5)
+
+        self.play(Transform(eq, eq2))
         self.wait()
